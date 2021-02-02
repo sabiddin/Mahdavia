60453279249b05c8c94083e4dca1ea1b2c01319f
sabiddin <abiddinsyed@gmail.com>
2021-02-02 13:17:24 -0500

    Added Http Client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { baseUrl } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Mahdvia app';
+  users: any = [];
+  /**
+   *
+   */
+  constructor(private _http: HttpClient) { }
+  ngOnInit(): void {
+    this._http.get(baseUrl + 'Users').subscribe( data =>
+      {
+      this.users = data;
+    }
+    ,    error =>
+    {
+      console.log(error);
+    });
+  }
+  
 }
